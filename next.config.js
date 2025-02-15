d630750e636d08b9b71257ded5682ad7f8214641
kaycmendes <kaycmendes@gmail.com>
2025-02-15 05:51:01 +0000

    update

--- next.config.js
+++ next.config.js
@@ -2,16 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   //add tailwind css
-  css: ['globals.css'],
   //add the tailwind config
-  tailwindConfig: './tailwind.config.js',
   //add the postcss config
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
+
 };
 
 module.exports = nextConfig; 
